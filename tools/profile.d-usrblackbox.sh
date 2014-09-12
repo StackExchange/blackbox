b7217e197dd4212c6e33c58c3cb8718616153d32
tlimoncelli@stackexchange.com <tlimoncelli@stackexchange.com>
2014-09-12 16:36:04 +0000

    Eliminate "mungepath". Improve performance and assure script will not touch disk.

--- tools/profile.d-usrblackbox.sh
+++ tools/profile.d-usrblackbox.sh
@@ -1,4 +1,2 @@
 x=/usr/blackbox/bin
-if type pathmunge>/dev/null 2>&1;then
-pathmunge $x
-else case ":$PATH:" in *:$x:*);;*)PATH="$x:$PATH";;esac;fi
+case ":$PATH:" in *:$x:*);;*)PATH="$x:$PATH";;esac
