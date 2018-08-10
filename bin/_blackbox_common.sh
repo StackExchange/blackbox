39afc47c762d25af277fbebff7ee2d6a674daa6a
Robert Savu <robert@kreditech.com>
2018-08-10 16:56:43 +0200

    changed paths of the form "/bin/[x]" into "/usr/bin/env [x]"

--- bin/_blackbox_common.sh
+++ bin/_blackbox_common.sh
@@ -140,7 +140,7 @@ function fail_if_not_on_cryptlist() {
 
   if ! is_on_cryptlist "$name" ; then
     echo "ERROR: $name not found in $BB_FILES" >&2
-    echo "PWD=$(/bin/pwd)" >&2
+    echo "PWD=$(/usr/bin/env pwd)" >&2
     echo 'Exiting...' >&2
     exit 1
   fi
--- blackbox.plugin.zsh
+++ blackbox.plugin.zsh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 # The MIT License (MIT)
 
 # Copyright (c) 2014 Stack Exchange, Inc.
