8c329b1305912565b904d1255f3f3ecf87611671
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2020-06-07 18:00:05 -0400

    linting

--- models/crypters.go
+++ models/crypters.go
@@ -1,6 +1,6 @@
 package models
 
-// CryptoSystem is gpg binaries, go-opengpg, etc.
+// Crypter is gpg binaries, go-opengpg, etc.
 type Crypter interface {
 	// Decrypt name+".gpg", possibly overwriting name.
 	Decrypt(filename string, overwrite bool) error
